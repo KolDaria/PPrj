5c3d53d96a5c08a3aa4251c6f1363cfd8e83e699
Daria Kolisnyk <dashamel17@gmail.com>
2024-12-24 18:18:43 +0300

    Реализованы функции filter_by_state, sort_by_date

---
+++ src/processing.py
@@ -0,0 +1,16 @@
+def filter_by_state(list_of_dicts: list, state: str ='EXECUTED') -> list:
+    """
+    Принимает список словарей и опционально значение для ключа и возвращает новый список словарей
+    """
+    container = []
+    for dicts in list_of_dicts:
+        if dicts['state'] == state:
+            container.append(dicts)
+    return container
+
+
+def sort_by_date(list_dicts_date: list, reverse: str ='True') -> list:
+    """
+    Принимает список словарей и параметр задающий порядок сортировки, возвращает новый отсортированный список
+    """
+    return sorted(list_dicts_date, key=lambda dicts: dicts['date'], reverse = reverse)
