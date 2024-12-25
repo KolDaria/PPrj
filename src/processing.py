b298da6a7fb32f33cd6d1c455a03c8d9e3c39216
Daria Kolisnyk <dashamel17@gmail.com>
2024-12-25 11:45:35 +0300

    Исправлена ошибка аннотации типов в функции sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -2,15 +2,15 @@ def filter_by_state(list_of_dicts: list, state: str = 'EXECUTED') -> list:
     """
     Принимает список словарей и опционально значение для ключа и возвращает новый список словарей
     """
-    container = []
+    new_lists = []
     for dicts in list_of_dicts:
         if dicts['state'] == state:
-            container.append(dicts)
-    return container
+            new_lists.append(dicts)
+    return new_lists
 
 
-def sort_by_date(list_dicts_date: list, reverse: str = 'True') -> list:
+def sort_by_date(list_dicts_date: list, reverse: bool = True) -> list:
     """
     Принимает список словарей и параметр задающий порядок сортировки, возвращает новый отсортированный список
     """
-    return sorted(list_dicts_date, key=lambda dicts: dicts['date'], reverse=reverse)
+    return sorted(list_dicts_date, key=lambda dicts_date: dicts_date['date'], reverse=reverse)
