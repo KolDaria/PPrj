bdef7eb26dd8f9ffe2eba4352cb0696f4d105b9f
Daria Kolisnyk <dashamel17@gmail.com>
2025-01-31 23:17:40 +0300

    Реализация функций преобразования файла в python объект, конвертация валюты

---
+++ external_api.py
@@ -0,0 +1,37 @@
+import os
+from typing import Any
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+
+
+def get_a_transaction_conversion(python_transaction: Any) -> Any:
+    """
+    Принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях
+    """
+    for transact in python_transaction:
+        if transact["operationAmount"]["currency"]["code"] != "RUB":
+            url = "https://api.apilayer.com/exchangerates_data/convert"
+            payload = {
+                "amount": transact["operationAmount"]["amount"],
+                "from": transact["operationAmount"]["currency"]["code"],
+                "to": "RUB"
+            }
+            headers = {
+                "apikey": f"{api_key}"
+            }
+
+            response = requests.get(url, headers=headers, params=payload)
+            status_code = response.status_code
+            result = response.json()
+            if status_code == 200:
+                conversion = round(float(result["result"]), 2)
+                print(conversion)
+            else:
+                print(f"Ошибка запроса. Причина: {response.reason}")
+        else:
+            amount = round(float(transact["operationAmount"]["amount"]), 2)
+            print(amount)
---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import json
+from typing import Any
+
+
+def get_list_dict_transaction(file_json: Any) -> Any:
+    """
+    Принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях
+    """
+    try:
+        with open(file_json, encoding='utf-8') as file_json:
+            data_json = json.load(file_json)
+            return data_json
+    except json.JSONDecodeError:
+        return list()
+    except FileNotFoundError:
+        return None
+
+
+file = 'C:\\Users\\Dr\\Desktop\\PPrj\\data\\operations.json'
+
+
+python_transaction = get_list_dict_transaction(file)
